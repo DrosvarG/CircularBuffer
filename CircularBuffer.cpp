b311dd8da4604b2f03260b62b2882ff4aacd93e7
CAP Fourrure <CAP Fourrure@DESKTOP-1LH2UM5>
2024-09-06 18:35:34 +0200

    Ajoutez des fichiers projet.

---
+++ CircularBuffer.cpp
@@ -0,0 +1,12 @@
+﻿// CircularBuffer.cpp : définit le point d'entrée de l'application.
+//
+
+#include "CircularBuffer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
